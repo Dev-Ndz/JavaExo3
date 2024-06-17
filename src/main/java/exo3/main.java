202dd32f10b243aca56cba61cad01fd9cda50d7b
ndz <nicolas.dzierzgowski@gmail.com>
2024-06-17 10:40:48 +0200

    Hello world

---
+++ src/main/java/exo3/main.java
@@ -0,0 +1,19 @@
+package exo3;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    Scanner scanner = new Scanner(System.in);
+	    String input;
+	    System.out.println("Welcome to Trade Data Analyzator");
+	    System.out.println("What data do you need ?");
+	    input = scanner.nextLine();
+	    if(input.equals("coucou"));
+	    System.out.println("coucou");
+	    scanner.close();
+	}
+
+}
